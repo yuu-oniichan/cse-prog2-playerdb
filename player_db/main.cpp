c8246abd97b60ff9cd7ae5d770a0b7c74314b4f4
yuu-oniichan <yuu.oniihan@gmail.com>
2016-04-23 17:00:30 +0800

    Change to instructor-given testcase2

--- player_db/main.cpp
+++ player_db/main.cpp
@@ -3,25 +3,26 @@
 
 int main()
 {
-	Database db;
-	Player jeremy, kobe, james, wade;
+	Database db1, db2;
+	Player jeremy, kobe, james;
 
 	strcpy_s(jeremy.name, "Jeremy");
 	strcpy_s(kobe.name, "Kobe");
 	strcpy_s(james.name, "James");
-	strcpy_s(wade.name, "Wade");
 
 	// ...
-	Create(&db); // capacity = size = 0
-	if (Add(&db, jeremy)) { printf("successful!"); } // use the return value of Add()
-	else { printf("Insufficient memory!"); }
-	Add(&db, kobe); // capacity = size = 2
-	Add(&db, james); // capacity = size = 3
-	Output(&db);
-	Remove(&db, Search(&db, "Jeremy")); // 假設自 db 成功刪除 jeremy
-	Output(&db); // 會列印兩筆資料，kobe 與 james; capacity = 3, size = 2
-	Add(&db, jeremy); // 這次呼叫不會配置記憶體; capacity = size = 3
-	Add(&db, wade); // 這次呼叫會配置記憶體; capacity = size = 4
-	Close(&db); // capacity = size = 0
+	Create(&db1);
+	Create(&db2);
+	Add(&db1, jeremy);
+	Add(&db1, kobe);
+	Add(&db2, james);
+	Output(&db2); // 會列印 james 的資料
+	if (Copy(&db2, &db1)) { printf("db1 is copied to db2 successfully.\n"); }
+	else { printf("db1 is not copied to db2 due to memory shortage.\n"); }
+	Add(&db1, james);
+	Remove(&db1, Search(&db1, "Jeremy")); // 假設自 db1 成功刪除 jeremy
+	Output(&db1); // 會列印兩筆資料，kobe 與 james
+	Output(&db2); // 會列印兩筆資料，jeremy 與 kobe
+	Close(&db1); Close(&db2);
 	return 0;
 }
